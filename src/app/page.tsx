47aae8a3cb8b8e8a8c2f354d4df3306321e80c18
Emanuel dos Santos <ernelemanuel10@hotmail.com>
2026-01-06 04:07:18 +0100

    ✨ feat(projects): add AngoLeague project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,11 +32,11 @@ const PROJECTS = [
     tags: ["Next.js", "Tailwind", "Framer Motion"], link: "https://sw-wanted.github.io/typemaster/", github: "https://github.com/SW-Wanted/typemaster"
   },
   { 
-    id: 2, title: "TERMINAL", category: "Terminal de Comando", 
-    img: "https://plus.unsplash.com/premium_photo-1681816189679-fa02d1acd1de?q=80&w=1177&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?w=800", 
-    bg: "https://plus.unsplash.com/premium_photo-1681816189679-fa02d1acd1de?q=80&w=1177&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?q=80&w=2000",
-    description: "Simulador de terminal de comando com funcionalidades básicas e personalização.",
-    tags: ["React", "Stripe", "Node.js"], link: "#", github: "#"
+    id: 2, title: "ANGOLEAGUE", category: "Futebol de Rua", 
+    img: "https://images.unsplash.com/photo-1510566337590-2fc1f21d0faa?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?w=800", 
+    bg: "https://images.unsplash.com/photo-1510566337590-2fc1f21d0faa?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?q=80&w=2000",
+    description: "Plataforma para organizar e gerenciar ligas de futebol de rua com sistema de rankings.",
+    tags: ["React", "Stripe", "Node.js"], link: "https://sw-wanted.github.io/AngoLeague/", github: "https://github.com/SW-Wanted/AngoLeague"
   },
   { 
     id: 3, title: "RIDING", category: "Serviço de Transporte", 
